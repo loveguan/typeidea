8962c3ff4e86e479b8143a360971dbfb0a0e3cdd
joj <zhouguanjie@qq.com>
2020-05-12 13:34:31 +0800

    eddd

--- blog/models.py
+++ blog/models.py
@@ -15,11 +15,14 @@ class Category(models.Model):
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices=STATUS_ITEMS, verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
-    owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -32,11 +35,14 @@ class Tag(models.Model):
     name = models.CharField(max_length=10, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices=STATUS_ITEMS, verbose_name='状态')
-    owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -53,9 +59,9 @@ class Post(models.Model):
     content = models.TextField(verbose_name='正文', help_text="正文必须为MarkDown格式")
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices=STATUS_ITEMS, verbose_name='状态')
-    category = models.ForeignKey(Category, verbose_name='分类',on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag, verbose_name='标签')
-    owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
