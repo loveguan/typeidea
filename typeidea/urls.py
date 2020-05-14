9b08e09425952b50d49c8c2d21d91e55fb4e3c74
joj <zhouguanjie@qq.com>
2020-05-14 15:55:05 +0800

    reverse url

--- typeidea/urls.py
+++ typeidea/urls.py
@@ -21,13 +21,12 @@ from blog.views import post_detail, post_list
 from config.views import links
 
 urlpatterns = [
-    re_path(r'^$', post_list),
-    re_path(r'^post/$',post_list),
-    re_path(r'^category/(?P<category_id>\d+)/$', post_list),
-    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    re_path(r'^post/(?P<post_id>\d+).html$', post_detail),
-    re_path(r'^links/$', links),
-    path('super_admin/', admin.site.urls),
-    path('admin/', custom_site.urls),
+    re_path(r'^$', post_list,name='index'),
+    re_path(r'^category/(?P<category_id>\d+)/$', post_list,name='category-list'),
+    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list,name='tag-list'),
+    re_path(r'^post/(?P<post_id>\d+).html$', post_detail,name='post-detail'),
+    re_path(r'^links/$', links,name='links'),
+    path('super_admin/', admin.site.urls,name='super-admin'),
+    path('admin/', custom_site.urls,name='admin'),
 
 ]
